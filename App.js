68d46687cda57dfaaca3d5181bded89a8f14e248
Netson Cavina <netsoncavina@gmail.com>
2023-01-17 00:26:20 -0300

    adicionadas paginas

--- App.js
+++ App.js
@@ -1,28 +1,51 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { useFonts } from "expo-font";
-import colors from "./assets/colors/colors";
+import react from "react";
+import { SafeAreaView, Text, View, StatusBar, Image } from "react-native";
+import AppIntroSlider from "react-native-app-intro-slider";
+import Onboard from "./components/Onboard";
 
-export default function App() {
-  const [fontsLoaded] = useFonts({
-    OpenSans: require("./assets/fonts/OpenSans-Regular.ttf"),
-    OpenSansBold: require("./assets/fonts/OpenSans-Bold.ttf"),
-    OpenSansSemiBold: require("./assets/fonts/OpenSans-SemiBold.ttf"),
-  });
+const data = [
+  {
+    title: "Save time by tracking your studies",
+    text: "Schedule your classes, assignments, quizzes and more",
+    image: require("./assets/images/Onboard1.png"),
+  },
+  {
+    title: "Stay on top of your education",
+    text: "Reduce your stress, increase your productivity",
+    image: require("./assets/images/Onboard2.png"),
+  },
+  {
+    title: "Spend more time doing the things you love",
+    text: "Get started within five minutes",
+    image: require("./assets/images/Onboard3.png"),
+  },
+];
+
+const App = () => {
+  const renderItem = ({ item }) => {
+    return (
+      <View>
+        <Image source={item.image} />
+        <View>
+          <Text>{item.title}</Text>
+          <Text>{item.text}</Text>
+        </View>
+      </View>
+    );
+  };
+
+  const keyExtractor = (item) => item.title;
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Funcionandooooo</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <StatusBar translucent backgroundColor="transparent" />
+      <AppIntroSlider
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+        data={data}
+      />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    fontFamily: "OpenSans",
-  },
-});
+export default App;
---
+++ components/Onboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Onboard = () => {
+  return (
+    <View>
+      <Text>Onboard</Text>
+    </View>
+  );
+};
+
+export default Onboard;
