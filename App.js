687f606af20d7d1dd9a11d2cb135c7e0dc7e27fb
Netson Cavina <netsoncavina@gmail.com>
2023-01-17 00:58:56 -0300

    adicionado condição para ir para Home

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import Onboard from "./components/Onboard";
+import { Home } from "./components/Home";
 
 export const App = () => {
   const [showOnBoard, setShowOnBoard] = useState(true);
-  return <>{showOnBoard && <Onboard />}</>;
+  return <>{showOnBoard ? <Onboard /> : <Home />}</>;
 };
 
 export default App;
