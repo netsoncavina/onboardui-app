5d627aa32d2b1aac9cd3de1fc599ceeb399df24c
Netson Cavina <netsoncavina@gmail.com>
2023-01-16 23:57:55 -0300

    adicionado import das cores

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
+import colors from "./assets/colors/colors";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
