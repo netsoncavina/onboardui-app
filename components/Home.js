e9470ce8f2291a999267b61cf311ebf0e68ad595
Netson Cavina <netsoncavina@gmail.com>
2023-01-17 00:57:51 -0300

    adicionado componente Home

---
+++ components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native-web";
+
+export const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
